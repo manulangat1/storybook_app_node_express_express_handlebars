ec7eda0ee2aa7a1af29c0d121c9c97f4eab2c2a0
manulangat <emmanuelthedeveloper@gmail.com>
2020-07-02 12:55:08 +0300

    ch:adding_DB_connection:backend

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const dotenv = require('dotenv')
 
+
+const connectDB = require('./config/db')
 //Load config 
 dotenv.config({path:'./config/config.env'})
 
+connectDB()
 
 const app = express()
+
+
 
 
 const PORT = process.env.PORT || 5000
