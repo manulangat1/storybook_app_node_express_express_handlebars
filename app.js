5a18ecfce3d1e2be288bd74ae1a09954ed58ecd7
manulangat <emmanuelthedeveloper@gmail.com>
2020-07-02 21:56:33 +0300

    ch:setting_up_routes:backend

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const dotenv = require('dotenv')
 const exphbs =  require('express-handlebars')
 const morgan = require('morgan')
+const path = require('path')
 const connectDB = require('./config/db')
 //Load config 
 dotenv.config({path:'./config/config.env'})
@@ -9,14 +10,19 @@ dotenv.config({path:'./config/config.env'})
 connectDB()
 
 const app = express()
-
+//Logginf 
 if(process.env.NODE_ENV === 'developement'){
     app.use(morgan('dev'))
-    console.log(`morgan`)
+    console.log(`How are you`)
 }
+//handlebars
 app.engine('.hbs',exphbs({defaultLayout:'main', extname:'.hbs'}))
 app.set('view engine','.hbs')
 
+// static folder
+app.use(express.static(path.join(__dirname,'public')))
+//Link to routes 
+app.use('/',require('./routes/index'))
 
 const PORT = process.env.PORT || 5000
 
