6295f3828276de824d8e7198e7a654c6bdda3baf
manulangat <emmanuelthedeveloper@gmail.com>
2020-07-05 22:22:21 +0300

    ch:finishing_authentication_part:backend

--- app.js
+++ app.js
@@ -2,13 +2,16 @@ const express = require('express')
 const dotenv = require('dotenv')
 const exphbs =  require('express-handlebars')
 const morgan = require('morgan')
+const passport = require('passport')
 const path = require('path')
+const session = require('express-session')
 const connectDB = require('./config/db')
 //Load config 
 dotenv.config({path:'./config/config.env'})
 
 connectDB()
-
+///passport config 
+require('./config/passport')(passport)
 const app = express()
 //Logginf 
 if(process.env.NODE_ENV === 'developement'){
@@ -18,11 +21,23 @@ if(process.env.NODE_ENV === 'developement'){
 //handlebars
 app.engine('.hbs',exphbs({defaultLayout:'main', extname:'.hbs'}))
 app.set('view engine','.hbs')
+//session
+app.use(
+    session({
+        secret:'jjdjjd',
+        resave:false,
+        saveUninitialized:false
+    })
+)
 
+//passport iddleware
+app.use(passport.initialize())
+app.use(passport.session())
 // static folder
 app.use(express.static(path.join(__dirname,'public')))
 //Link to routes 
 app.use('/',require('./routes/index'))
+app.use('/auth',require('./routes/auth'))
 
 const PORT = process.env.PORT || 5000
 
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
 const express = require('express')
-
+const {ensureAuth,ensureGuest} = require('../middleware/auth')
 const router = express.Router()
 
 // @desc Login/Landing page
 // @route GET /
 
-router.get('/',(req,res) => {
+router.get('/',ensureGuest, (req,res) => {
     res.render('Login',{
         layout:'login'
     })
@@ -14,7 +14,7 @@ router.get('/',(req,res) => {
 
 // @desc Dashboard
 // @route GET /dashboard
-router.get('/dashboard',(req,res) => {
+router.get('/dashboard',ensureAuth,(req,res) => {
     res.render('Dashboard')
 })
 module.exports = router 
